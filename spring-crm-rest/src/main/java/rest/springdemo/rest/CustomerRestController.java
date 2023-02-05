4ff28a3ead091a5f36654e2f1f7d0b5b4431cfe5
spiridijlqk2 <martivr98@gmail.com>
2023-02-05 19:27:48 +0200

    Added security support

--- spring-crm-rest/src/main/java/rest/springdemo/rest/CustomerRestController.java
+++ spring-crm-rest/src/main/java/rest/springdemo/rest/CustomerRestController.java
@@ -18,79 +18,78 @@ import rest.springdemo.service.CustomerService;
 @RestController
 @RequestMapping("/api")
 public class CustomerRestController {
-	
-	//autowire customer service
+
+	// autowire the CustomerService
 	@Autowired
 	private CustomerService customerService;
 	
-	//add mapping for GET /customers
+	// add mapping for GET /customers
 	@GetMapping("/customers")
-	public List<Customer> getCustomers(){
+	public List<Customer> getCustomers() {
+		
 		return customerService.getCustomers();
+		
 	}
+	
+	// add mapping for GET /customers/{customerId}
 	
 	@GetMapping("/customers/{customerId}")
 	public Customer getCustomer(@PathVariable int customerId) {
 		
 		Customer theCustomer = customerService.getCustomer(customerId);
 		
-		if(theCustomer == null) {
+		if (theCustomer == null) {
 			throw new CustomerNotFoundException("Customer id not found - " + customerId);
 		}
 		
 		return theCustomer;
 	}
 	
-	// add mapping for POST /customers - add new customer
+	// add mapping for POST /customers  - add new customer
+	
 	@PostMapping("/customers")
 	public Customer addCustomer(@RequestBody Customer theCustomer) {
 		
-		//also just in case the passed ID is json... set id to 0.. to save instaed of update
+		// also just in case the pass an id in JSON ... set id to 0
+		// this is force a save of new item ... instead of update
 		
 		theCustomer.setId(0);
+		
 		customerService.saveCustomer(theCustomer);
 		
 		return theCustomer;
 	}
 	
-	//add mapping for PUT /customers - update customer
+	// add mapping for PUT /customers - update existing customer
+	
 	@PutMapping("/customers")
 	public Customer updateCustomer(@RequestBody Customer theCustomer) {
 		
 		customerService.saveCustomer(theCustomer);
+		
 		return theCustomer;
 		
 	}
 	
-	//add mapping for DELETE /customer/{customerId} - delete customer
+	// add mapping for DELETE /customers/{customerId} - delete customer
 	
 	@DeleteMapping("/customers/{customerId}")
 	public String deleteCustomer(@PathVariable int customerId) {
 		
 		Customer tempCustomer = customerService.getCustomer(customerId);
 		
-		//throw exception if customerid is null
-		if(tempCustomer == null) {
+		// throw exception if null
+		
+		if (tempCustomer == null) {
 			throw new CustomerNotFoundException("Customer id not found - " + customerId);
 		}
-		
+				
 		customerService.deleteCustomer(customerId);
 		
 		return "Deleted customer id - " + customerId;
 	}
 	
-
 }
-
-
-
-
-
-
-
-
-
-
 
 
 
